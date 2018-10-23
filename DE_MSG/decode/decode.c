241b38165e6c115e3813a36b7b310e4beee0fe5e
LikeShadows <huihang0826@126.com>
2018-10-23 19:37:48 +0800

    已完成：     1. 添加 nanopb API 接口.     2. 添加 messages.* 文件.     3. 添加消息处理函数. 待完成：     1. 编写消息处理函数逻辑, 实现消息解码部分.
    遗留问题：
        目前在消息处理函数中还没获取到存放消息的全局变量 g_DataBuf, 下一步需要先获取到该变量.

---
+++ DE_MSG/decode/decode.c
@@ -0,0 +1,21 @@
+#include "decode.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+BOOL decoding_Initialize(UINT g_Msg_Size)
+{
+
+
+
+	return TRUE;
+}
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
---
+++ DE_MSG/typedef.h
@@ -0,0 +1,59 @@
+#ifndef _MINWINDEF_
+#define _MINWINDEF_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifndef NULL
+#ifdef __cplusplus
+#define NULL    0
+#else
+#define NULL    ((void *)0)
+#endif
+#endif
+
+#ifndef FALSE
+#define FALSE               0
+#endif
+
+#ifndef TRUE
+#define TRUE                1
+#endif
+
+typedef int                 BOOL;
+typedef char                CHAR;
+typedef signed char         INT8;
+typedef unsigned char       UCHAR;
+typedef unsigned char       UINT8;
+typedef unsigned char       BYTE;
+typedef short               SHORT;
+typedef signed short        INT16;
+typedef unsigned short      USHORT;
+typedef unsigned short      UINT16;
+typedef unsigned short      WORD;
+typedef int                 INT;
+typedef signed int          INT32;
+typedef unsigned int        UINT;
+typedef unsigned int        UINT32;
+typedef long                LONG;
+typedef unsigned long       ULONG;
+typedef unsigned long       DWORD;
+typedef __int64             LONGLONG;
+typedef __int64             LONG64;
+typedef signed __int64      INT64;
+typedef unsigned __int64    ULONGLONG;
+typedef unsigned __int64    DWORDLONG;
+typedef unsigned __int64    ULONG64;
+typedef unsigned __int64    DWORD64;
+typedef unsigned __int64    UINT64;
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif 
+
+
+
